0dafa5c916ac09918710ccbd6765690bc9a33ef9
Randy Morgan <morgan13231323@yahoo.com>
2020-08-11 15:50:12 -0500

    Done

---
+++ BlindDating/BlindDating/Models/SecurityContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.CodeAnalysis.Options;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BlindDating.Models
+{
+    public class SecurityContext : IdentityDbContext
+    {
+        public SecurityContext()
+        {
+        }
+        public SecurityContext(DbContextOptions<SecurityContext> options)
+             : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-EQ5TUGB\\SQLEXPRESS;Database=BlindDating;Trusted_Connection=True;");
+            }
+        }
+    }
+}
